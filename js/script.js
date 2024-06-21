9ec546084af0f9a0465459bc3ea5b1807bcddf49
metasebiyaw asfaw <136433184+chapi1234@users.noreply.github.com>
2024-06-21 11:47:06 -0900

    add project list page

--- js/script.js
+++ js/script.js
@@ -29,3 +29,24 @@ function popupDisplayNone(id) {
       element.style.transition = "0.3s";
   }
 }
+const menubtn = document.getElementById('menubtn');
+const menu = document.getElementById('menu');
+const gearTurn = document.getElementById('gear-turn');
+const menuTurnBtn = document.getElementById('menu-trun-btn');
+menu.style.transform = 'translateX(200px)';
+menuTurnBtn.style.transform = 'rotateZ(180deg)';
+
+menubtn.addEventListener('click', () => {
+  gearTurn.style.transform = gearTurn.style.transform === 'rotateZ(360deg)' ? 'rotateZ(0)' : 'rotateZ(360deg)';
+  menuTurnBtn.style.transform = menuTurnBtn.style.transform === 'rotateZ(180deg)' ? 'rotateZ(0)' : 'rotateZ(180deg)';
+  menu.style.transform = menu.style.transform === 'translateX(200px)' ? 'translateX(0)' : 'translateX(200px)';
+});
+      
+
+document.addEventListener('click', (event) => {
+  if (!menubtn.contains(event.target) && !menu.contains(event.target) && !gearTurn.contains(event.target)) {
+      menu.style.transform = 'translateX(200px)';
+      gearTurn.style.transform = 'rotateZ(0)';
+      menuTurnBtn.style.transform = 'rotateZ(180deg)';
+  }
+});
